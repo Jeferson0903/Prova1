e4362d52b7ab008c22accc351ff2ecab86c83644
Jeferson0903 <infortec0104@gmail.com>
2024-10-01 20:24:07 -0300

    Quinto commit

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Todo
 
-# Create your views here.
+def todo_list(request):
+    todos = Todo.objects.all()  # Obtém todos os objetos Todo
+    return render(request, 'todos/todo_list.html', {'todos': todos})
